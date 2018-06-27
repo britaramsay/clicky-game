e2aec74b9e88909b916529de967abe6409b7ea61
Brita Ramsay <britaramsay@gmail.com>
2018-06-27 18:26:03 -0400

    top score added

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -7,7 +7,8 @@ class Container extends Component {
     state = {
         images: images,
         clickedImages: [],
-        score: 0
+        score: 0,
+        topScore: 0
     }
 
     addClicked = (id) => {
@@ -20,17 +21,19 @@ class Container extends Component {
             if(this.state.score === 11) {
                 alert("WINNER!")
                 this.setState({score: 0, clickedImages: []})
+                if(this.state.topScore !== 12)
+                    this.setState({topScore: 12})
             } 
         }
         else {
+            if(this.state.score > this.state.topScore)
+                this.setState({topScore: this.state.score})
             this.setState({score: 0, clickedImages: []})
         }
         console.log(clicked)
-
     }
 
     randomizeImages = (e) => {
-        // this.state.images.forEach(x => console.log(x))
         this.addClicked(e.target.id)
 
         var newImages = [],
@@ -42,17 +45,14 @@ class Container extends Component {
             this.state.images.splice(randomIndex, 1)
             arrLength--;
         }
-        // newImages.forEach(x => console.log(x))
-
         this.setState({images: newImages})
         this.state.images.forEach(x => console.log(x))
-
     }
 
     render () { 
         return (
             <div className="container">
-                <p>{this.state.score}</p>
+                <h3>Score: {this.state.score} | Top Score: {this.state.topScore}</h3>
                 <Row random={this.randomizeImages} one={this.state.images[0]} two={this.state.images[1]} three={this.state.images[2]} four={this.state.images[3]}/>
                 <Row random={this.randomizeImages} one={this.state.images[4]} two={this.state.images[5]} three={this.state.images[6]} four={this.state.images[7]}/>
                 <Row random={this.randomizeImages} one={this.state.images[8]} two={this.state.images[9]} three={this.state.images[10]} four={this.state.images[11]}/>                         
