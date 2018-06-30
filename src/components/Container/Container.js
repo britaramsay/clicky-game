aeb7149fdc9fd49fb16ac968a5c2b2ac982e2e47
Brita Ramsay <britaramsay@gmail.com>
2018-06-29 21:39:34 -0400

    alerts added

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,30 +2,39 @@ import React, { Component } from "react";
 import Row from "../Row/Row.js";
 import './container.css';
 import images from '../../images.json';
+import { Alert } from 'reactstrap';
 
 class Container extends Component {
     state = {
         images: images,
         clickedImages: [],
         score: 0,
-        topScore: 0
+        topScore: 0,
+        alertType: '',
+        alertText: ''
     }
 
     addClicked = (id) => {
+        this.setState({alertType: '', alertText: ''})
         let clicked = this.state.clickedImages
         if(clicked.indexOf(id) === -1) {
+            this.setState({alertType: 'info', alertText: 'You guessed correctly'})
+
             clicked.push(id)
             let newScore = this.state.score + 1;
             this.setState({score: newScore})
 
             if(this.state.score === 11) {
-                alert("WINNER!")
+                // alert("WINNER!")
+                this.setState({alertType: 'success', alertText: 'You Won!'})
+
                 this.setState({score: 0, clickedImages: []})
                 if(this.state.topScore !== 12)
                     this.setState({topScore: 12})
             } 
         }
         else {
+            this.setState({alertType: 'danger', alertText: 'You guessed incorrectly'})
             if(this.state.score > this.state.topScore)
                 this.setState({topScore: this.state.score})
             this.setState({score: 0, clickedImages: []})
@@ -52,7 +61,11 @@ class Container extends Component {
     render () { 
         return (
             <div className="container">
-                <h3>Score: {this.state.score} | Top Score: {this.state.topScore}</h3>
+                <br/>
+                <Alert color={this.state.alertType}>
+                    {this.state.alertText}
+                </Alert>
+                <h3>Score: {this.state.score} | Top Score: {this.state.topScore}</h3>                
                 <Row random={this.randomizeImages} one={this.state.images[0]} two={this.state.images[1]} three={this.state.images[2]} four={this.state.images[3]}/>
                 <Row random={this.randomizeImages} one={this.state.images[4]} two={this.state.images[5]} three={this.state.images[6]} four={this.state.images[7]}/>
                 <Row random={this.randomizeImages} one={this.state.images[8]} two={this.state.images[9]} three={this.state.images[10]} four={this.state.images[11]}/>                         
